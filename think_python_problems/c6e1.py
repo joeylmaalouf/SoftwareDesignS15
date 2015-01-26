6516f70d6c2b05c3b7bb0c2497ea81176ec794b4
Joey L. Maalouf <joey.l.maalouf@gmail.com>
2015-01-26 16:32:19 -0500

    did c6e1

---
+++ think_python_problems/c6e1.py
@@ -0,0 +1,21 @@
+""" Write a compare function that returns
+ 1 if x >  y
+ 0 if x == y
+-1 if x <  y
+"""
+
+import sys
+
+
+def compare(x, y):
+	return 0 if x == y else (1 if x > y else -1)
+
+
+def main(argv):
+	print(compare(1, 0))
+	print(compare(0, 0))
+	print(compare(0, 1))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
