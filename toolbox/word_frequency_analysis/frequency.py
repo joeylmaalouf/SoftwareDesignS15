ae4d182dfa1e1575dd605c8ff70d9863fcd2784c
joeylmaalouf <joeylmaalouf@gmail.com>
2015-02-20 16:29:14 -0500

    did the word frequency toolbox assignment

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -1,7 +1,10 @@
-""" Analyzes the word frequencies in a book downloaded from
-	Project Gutenberg """
-
+""" Analyzes the word frequencies in a book downloaded from Project Gutenberg
+	Joey L. Maalouf
+"""
+import operator
 import string
+import sys
+
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -9,7 +12,20 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	with open(file_name, "r") as readfile:
+		#  read the file contents into a string
+		text = readfile.read()
+		#  strip everything before the start marker
+		text = text[text.find("START OF THIS PROJECT GUTENBERG EBOOK"):]
+		#  remove the start marker by trimming to the next newline
+		text = text[text.find("\n"):]
+		#  make case-insensitive by turning everything lowercase
+		text = text.lower()
+		#  remove all punctuation by "translating" it to empty strings
+		text = text.translate(string.maketrans("", ""), string.punctuation)
+		#  split, by default, splits on any whitespace character, leaving us with a list of just words
+		return text.split()
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -19,6 +35,25 @@ def get_top_n_words(word_list, n):
 					punctuation
 		n: the number of words to return
 		returns: a list of n most frequently occurring words ordered from most
-				 frequently to least frequentlyoccurring
+				 frequently to least frequently occurring
 	"""
-	pass
+	word_dict = {}
+	for word in word_list:
+		if word in word_dict:
+			#  add to its existing count
+			word_dict[word] += 1
+		else:
+			#  initialize its starting count
+			word_dict[word] = 1
+	#  return a list of tuples (to represent the dictionary) of length n, reversely sorted by value (occurence count)
+	return sorted(word_dict.items(), key = operator.itemgetter(1), reverse = True)[:n]
+
+
+def main(argv):
+	top_words = get_top_n_words(get_word_list("./pg10148.txt"), 100)
+	for pair in top_words:
+		print(pair)
+
+
+if __name__ == "__main__":
+	main(sys.argv)
