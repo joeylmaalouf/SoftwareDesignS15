cc0a28dc01ea60f4200337bea19bc1144379aab5
joeylmaalouf <joeylmaalouf@gmail.com>
2015-02-20 17:44:09 -0500

    did algorithm music toolbox assignment

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -5,19 +5,19 @@ import numpy as np
 from random import choice
 
 def add_note(out, instr, key_num, duration, bpm, volume):
-    """ Adds a note from the given instrument to the specified stream
+	""" Adds a note from the given instrument to the specified stream
 
-        out: the stream to add the note to
-        instr: the instrument that should play the note
-        key_num: the piano key number (A 440Hzz is 49)
-        duration: the duration of the note in beats
-        bpm: the tempo of the music
-        volume: the volume of the note
+		out: the stream to add the note to
+		instr: the instrument that should play the note
+		key_num: the piano key number (A 440Hzz is 49)
+		duration: the duration of the note in beats
+		bpm: the tempo of the music
+		volume: the volume of the note
 	"""
-    freq = (2.0**(1/12.0))**(key_num-49)*440.0
-    stream = instr.play(duration*(60.0/bpm),freq)
-    stream *= volume
-    out << stream
+	freq = (2.0**(1/12.0))**(key_num-49)*440.0
+	stream = instr.play(duration*(60.0/bpm), freq)
+	stream *= volume
+	out << stream
 
 # this controls the sample rate for the sound file you will generate
 sampling_rate = 44100.0
@@ -33,4 +33,31 @@ beats_per_minute = 45				# Let's make a slow blues solo
 
 add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
 
-solo >> "blues_solo.wav"
+curr_note = 0
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, 1.0)
+
+licks = [
+	[ [ 1, 0.5], [ 1, 0.5], [ 1, 0.5], [ 1, 0.5] ],
+	[ [ 1, 0.5], [ 1, 0.5], [-1, 0.5], [-1, 0.5] ],
+	[ [-1, 0.5], [-1, 0.5], [ 1, 0.5], [ 1, 0.5] ],
+	[ [-1, 0.5], [-1, 0.5], [-1, 0.5], [-1, 0.5] ],
+	[ [ 1, 1.0], [ 1, 1.0], ],
+	[ [ 1, 1.0], [-1, 1.0], ],
+	[ [-1, 1.0], [ 1, 1.0], ],
+	[ [-1, 1.0], [-1, 1.0], ]
+	]
+for i in range(20):
+	lick = choice(licks)
+	for note in lick:
+		#  keep index within bounds
+		curr_note = max(0, min(len(blues_scale)-1, curr_note+note[0]))
+		add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, 1.0)
+
+backing_track = AudioStream(sampling_rate, 1)
+Wavefile.read('backing.wav', backing_track)
+m = Mixer()
+solo *= 0.75  #  adjust relative volumes to taste
+backing_track *= 2.0
+m.add(2.25, 0, solo)  #  delay the solo to match up with backing track   
+m.add(0, 0, backing_track)
+m.getStream(500.0) >> "slow_blues.wav"
