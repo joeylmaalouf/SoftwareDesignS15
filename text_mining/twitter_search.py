84c02e6e7ff99e8a8c157e9473f548fda99fa9ee
joeylmaalouf <joeylmaalouf@gmail.com>
2015-02-22 21:37:20 -0500

    added optional count input

--- text_mining/twitter_search.py
+++ text_mining/twitter_search.py
@@ -3,9 +3,12 @@ from sys import argv
 
 
 def main(argv):
+	if len(argv) < 3:
+		argv.append(10)
 	t = Twitter()
 	i = None
-	for tweet in t.search(argv[1], start=i, count=10):
+	print("")
+	for tweet in t.search(argv[1], start = i, count = int(argv[2])):
 		print(tweet.text)
 		print("")
 		i = tweet.id
