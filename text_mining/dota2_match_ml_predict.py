9c3a9684010a41970449083c1bec5e4c35dca038
joeylmaalouf <joeylmaalouf@gmail.com>
2015-02-25 00:01:51 -0500

    done?

--- text_mining/dota2_match_ml_predict.py
+++ text_mining/dota2_match_ml_predict.py
@@ -1,8 +1,8 @@
 """ Defense of the Ancients 2 Match Machine Learning Prediction
 	This program reads in the data file created by the match analysis and
-	uses the hero matchup information to train a neural network. Hopefully,
-	given any 5v5 set of hero ids, the network will be able to predict which
-	team will win based on the hero matchups.
+	uses the hero matchup information to train some machine learning model.
+	Hopefully, given any 5v5 set of hero ids, the model will be able to
+	predict which team will win based on the hero matchups.
 
 	Joey L. Maalouf
 """
@@ -13,15 +13,28 @@ from cPickle     import load
 from numpy       import array
 from os.path     import exists
 from pprint      import pprint
+from random      import sample
 from sklearn.svm import SVC
 from sys         import argv
 
 
-def parse(data, x, y):
+def split_data(data):
+	""" Given a list of data, randomly select 3/4 of it
+		as training data and the rest as testing data.
+	"""
+	num_train = 3*len(data)/4
+	shuffled = sample(data, len(data))
+	return (shuffled[num_train:], shuffled[:num_train])
+
+
+def process(data):
+	""" Given a list of data, return two lists of
+		input features (x) and corresponding output (y).
+	"""
+	x, y = [], []
 	for game in data:
 		x.append([float(player["hero_id"]) for player in game["players"]])
 		y.append(game["radiant_win"])
-	data = zip(x, y)
 	try:
 		for i in range(len(x)):
 			if len(x[i]) < 10:
@@ -29,34 +42,31 @@ def parse(data, x, y):
 				del y[i]
 	except IndexError:
 		pass
+	return (x, y)
 
 
 def main(argv):
+	#  ----  LOAD DATA  --------------------------------------------------------
 	data_file = "dota2_match_data_list.txt"
 	game_details = {}
 	if exists(data_file):
 		file_object = open(data_file, "rb")
 		game_details = load(file_object)
 		file_object.close()
+	print(str(len(game_details))+" samples")
 
-	size = len(game_details)
-	print(str(size)+" samples")
-	train_xy = game_details.values()[:-size/4]
-	train_x, train_y = [], []
-	test_xy = game_details.values()[-size/4:]
-	test_x, test_y = [], []
+	#  ----  PROCESS DATA  -----------------------------------------------------
+	(train_xy, test_xy) = split_data(game_details.values())
+	(train_x, train_y) = process(train_xy)
+	(test_x, test_y) = process(test_xy)
 
-	parse(train_xy, train_x, train_y)
-	parse(test_xy, test_x, test_y)
 
-	train_x = array(train_x)
-	train_y = array(train_y)
-	test_x = array(test_x)
-	test_y = array(test_y)
-
+	#  ----  CREATE MODEL  -----------------------------------------------------
 	svc = SVC(kernel = "linear")
 	svc.fit(train_x, train_y)
 	prediction = svc.predict(test_x)
+	
+	#  ----  GET OUTPUT  -------------------------------------------------------
 	results = [int(prediction[i] == test_y[i]) for i in range(len(prediction))]
 	accuracy = float(sum(results))/len(results)
 	print("{:0.2f}".format(accuracy*100)+"% accuracy")
