fda3d237cc5c767246d379cffdf3e82a17260853
joeylmaalouf <joeylmaalouf@gmail.com>
2015-02-25 22:50:22 -0500

    did ml toolbox

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -1,4 +1,7 @@
-""" Exploring learning curves for classification of handwritten digits """
+""" Exploring learning curves for classification of handwritten digits
+	Joey L. Maalouf
+"""
+
 
 import matplotlib.pyplot as plt
 import numpy
@@ -6,21 +9,24 @@ from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-data = load_digits()
-print data.DESCR
-num_trials = 10
-train_percentages = range(5,95,5)
+num_trials = 50
+train_percentages = range(5, 95, 5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
-# train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
-# the resultant accuracy.
-# You should repeat each training percentage num_trials times to smooth out variability
-# for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
+data = load_digits()
+print(data.DESCR)
 
-# TODO: your code here
+for i in range(len(train_percentages)):
+	sum_score = 0.0
+	for j in range(num_trials):
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percentages[i]/100.0)
+		model = LogisticRegression(C = 1.0)
+		model.fit(X_train, y_train)
+		sum_score += model.score(X_test, y_test)
+	test_accuracies[i] = sum_score/num_trials
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
-plt.xlabel('Percentage of Data Used for Training')
-plt.ylabel('Accuracy on Test Set')
+plt.xlabel("Percentage of Data Used for Training")
+plt.ylabel("Accuracy on Test Set")
 plt.show()
